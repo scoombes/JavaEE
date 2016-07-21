e1eecfdc691c83d191f34b15eac87def03b081aa
John Steel <john@steelcomputers.com>
2016-07-21 13:29:03 -0400

    ScheduleHIO

---
+++ JS_SC_JaveEE/src/com/seanjohn/nhl/data/ScheduleHIO.java
@@ -0,0 +1,41 @@
+/*
+ *   Document   : ScheduleIO.java
+ *   Created on : June 15, 2016
+ *   Authors    : John Steel & Sean Coombes
+ */
+
+package com.seanjohn.nhl.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.seanjohn.nhl.business.Game;
+import com.seanjohn.nhl.business.Roster;
+
+/**
+ * @author black
+ *
+ */
+public class ScheduleHIO extends HibernateIO {
+	public List<Game> getCompletedGames() throws SQLException {
+		return em
+				.createQuery(
+						"SELECT g "
+								+ "FROM Game g "
+								+ "WHERE g.homeScore IS NOT null AND g.visitorScore IS NOT null "
+								+ "ORDER BY g.gameDate", Game.class)
+				.getResultList();
+	}
+
+	public List<Game> getUpcomingGames() throws SQLException {
+		return em
+				.createQuery(
+						"SELECT g "
+								+ "FROM Game g "
+								+ "WHERE g.homeScore IS null AND g.visitorScore IS null "
+								+ "ORDER BY g.gameDate", Game.class)
+				.getResultList();
+	}
+}
