1e72bb802b5e91bb9df624c9008148a5ab3dbce7
Sean Coombes <seancoombes@live.ca>
2016-08-01 13:33:59 -0400

    added gamesession EJB

---
+++ JS_SC_JaveEE/src/com/seanjohn/nhl/business/GameSessionBean.java
@@ -0,0 +1,14 @@
+package com.seanjohn.nhl.business;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class GameSessionBean {
+	
+	public String getScore(Game game, Team Team){
+		
+		
+		
+		return "";
+	}
+}
