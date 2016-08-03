a7b32eaae8bf097732f46fa622dc95bdd211680c
John Steel <john@steelcomputers.com>
2016-08-02 20:11:56 -0400

    Date formated

--- JS_SC_JaveEE/src/com/seanjohn/nhl/business/ScheduleSessionBean.java
+++ JS_SC_JaveEE/src/com/seanjohn/nhl/business/ScheduleSessionBean.java
@@ -3,25 +3,32 @@
  */
 package com.seanjohn.nhl.business;
 
+import java.text.SimpleDateFormat;
+
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
 
 @Stateless
 public class ScheduleSessionBean {
-
+	
 	/**
 	 * 
 	 */
 	public ScheduleSessionBean() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	public String getNextGame(Team team, Schedule schedule) {
+		
+		String pattern = "EEE d MMM yyyy";
+		SimpleDateFormat format = new SimpleDateFormat(pattern);
+		
 		String teamID = team.getTeamId();
 		for (Game game : schedule.getGames()) {
 			if (game.getHome().getTeamId().equals(teamID) || game.getVisitor().getTeamId().equals(teamID)) {
-				return game.getGameDate().toString();
+				return format.format(game.getGameDate()).toUpperCase();
 			}
 		}
-		return team.getTeamname() + " Had no games?!?";
+		
+		return "No games remaining";
 	}
 }
